33a8ef65ef8ca37bccfae4b0c14fa226b25cc8e9
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-06-03 12:43:17 +0200

    feat(external-footer): add external footer component

---
+++ packages/external-footer/src/components/footer/index.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from 'react';
+import Link from '@fellesdatakatalog/link';
+
+import EmailIcon from '../../images/email.svg';
+import TwitterIcon from '../../images/twitter.svg';
+
+import SC from './styled';
+
+const Footer: FC = () => (
+  <SC.Footer>
+    <SC.Illustration />
+    <SC.Content>
+      <SC.Column>
+        <h6>Søk i kataloger</h6>
+        <Link href='/search-all'>Søk i alle kataloger</Link>
+        <Link href='/datasets'>Søk i datasett</Link>
+        <Link href='/apis'>Søk i datatjenester</Link>
+        <Link href='/concepts'>Søk i begreper</Link>
+        <Link href='/informationmodels'>Søk i informasjonsmodeller</Link>
+      </SC.Column>
+      <SC.Column>
+        <h6>Om Felles datakatalog</h6>
+        <Link href='https://data.norge.no/guidance'>
+          Veiledere og standarder
+        </Link>
+        <Link href='https://data.norge.no/reports'>Rapporter</Link>
+        <Link href='https://data.norge.no/organizations'>Virksomheter</Link>
+        <Link href='https://data.norge.no/about'>Om Felles datakatalog</Link>
+        <Link href='https://data.norge.no/about-registration'>
+          Hjelp til å registrere
+        </Link>
+        <Link href='https://data.norge.no/news-archive'>Aktuelt</Link>
+      </SC.Column>
+      <SC.Column>
+        <h6>Om nettstedet</h6>
+        <p>Digitaliseringsdirektoratet forvalter Felles datakatalog.</p>
+        <Link href='https://www.digdir.no/om-oss/personvernerklaering/706'>
+          Personvernerklæring
+        </Link>
+        <Link href='https://www.digdir.no/om-oss/informasjonskapsler/707'>
+          Informasjonskapsler
+        </Link>
+      </SC.Column>
+      <SC.Column>
+        <h6>Kontakt</h6>
+        <Link href='mailto:fellesdatakatalog@digdir.no' icon={<EmailIcon />}>
+          fellesdatakatalog@digdir.no
+        </Link>
+        <Link
+          href='https://twitter.com/datakatalogen?ref_src=twsrc%5Etfw%7Ctwcamp%5Eembeddedtimeline%7Ctwterm%5Eprofile%3Adatakatalogen&ref_url=https%3A%2F%2Ffellesdatakatalog.digdir.no%2F'
+          icon={<TwitterIcon />}
+          external
+        >
+          Felles datakatalog på Twitter
+        </Link>
+      </SC.Column>
+    </SC.Content>
+  </SC.Footer>
+);
+
+export default Footer;
---
+++ packages/external-footer/src/components/footer/styled.ts
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+import { theme, Colour } from '@fellesdatakatalog/theme';
+
+import FooterIllustrationSVG from '../../images/footer-illustration.svg';
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  min-height: 200px;
+  padding: ${theme.spacing('S48')} 0;
+  font-size: ${theme.fontSize('FS14')};
+  background: ${theme.colour(Colour.NEUTRAL, 'N60')};
+  color: ${theme.colour(Colour.NEUTRAL, 'N0')};
+`;
+
+const Illustration = styled(FooterIllustrationSVG)`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 100%;
+  transform: rotate(180deg);
+
+  @media screen and (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1140px;
+  z-index: 1;
+
+  @media screen and (max-width: 1500px) {
+    max-width: 600px;
+  }
+
+  @media screen and (max-width: 696px) {
+    padding: 0 ${theme.spacing('S48')};
+    max-width: 100%;
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  flex: 1;
+
+  & > *:nth-child(n + 3) {
+    margin-top: ${theme.spacing('S12')};
+  }
+
+  & > h6 {
+    margin-bottom: ${theme.spacing('S16')};
+    font-size: ${theme.fontSize('FS16')};
+    font-weight: ${theme.fontWeight('FW700')};
+  }
+
+  @media screen and (max-width: 1500px) {
+    flex-basis: 50%;
+
+    &:nth-of-type(n + 3) {
+      margin-top: ${theme.spacing('S32')};
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    flex-shrink: 0;
+    flex-basis: 100%;
+
+    &:nth-of-type(n + 2) {
+      margin-top: ${theme.spacing('S32')};
+    }
+  }
+`;
+
+export default { Footer, Illustration, Content, Column };
