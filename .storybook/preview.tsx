94391d38e13b367dd150385a7be82d375fde4499
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-10-22 12:51:48 +0200

    chore: remove Frame from storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { StyleSheetManager } from 'styled-components';
-import { iframeResizer } from 'iframe-resizer';
-import Frame, { FrameContextConsumer } from 'react-frame-component';
 import { addDecorator, addParameters } from '@storybook/react';
 
-import ThemeProvider from '../packages/theme/src';
+import ThemeProvider, { theme } from '../packages/theme/src';
 
 addParameters({
   viewMode: 'docs',
@@ -17,39 +14,8 @@ addParameters({
   }
 });
 
-addDecorator((story, { id }) => {
-  const frameId = `story-iframe-${id}`;
-
-  const injectScript = document => {
-    const resizerScriptElement = document.createElement('script');
-    resizerScriptElement.type = 'text/javascript';
-    resizerScriptElement.src =
-      './storybook-static/iframeResizer.contentWindow.min.js';
-    document.head.appendChild(resizerScriptElement);
-  };
-
-  document.head
-    .querySelectorAll('style[data-fonts]')
-    .forEach(style => style.remove());
-
-  return (
-    <Frame
-      id={frameId}
-      style={{ height: '0', width: '1px', minWidth: '100%' }}
-      frameBorder={0}
-    >
-      <FrameContextConsumer>
-        {({ document }) => {
-          injectScript(document);
-          iframeResizer({ log: false, checkOrigin: false }, `#${frameId}`);
-
-          return (
-            <StyleSheetManager target={document.head}>
-              <ThemeProvider target={document.head}>{story()}</ThemeProvider>
-            </StyleSheetManager>
-          );
-        }}
-      </FrameContextConsumer>
-    </Frame>
-  );
-});
+addDecorator(story => (
+  <ThemeProvider>
+    <div style={{ fontFamily: theme.fontFamily()({}) }}>{story()}</div>
+  </ThemeProvider>
+));
