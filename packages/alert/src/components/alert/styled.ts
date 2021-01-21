e60b51659f6c70e471b46d1c9a3f78d2d10e1b65
Jeff Reiffers <jeffreiffers@gmail.com>
2021-01-21 10:40:55 +0100

    fix: increased fontsize and color fix

--- packages/alert/src/components/alert/styled.ts
+++ packages/alert/src/components/alert/styled.ts
@@ -15,28 +15,41 @@ const Alert = styled.div<Props>`
   display: flex;
   padding: 1em 1em;
   font-family: ${theme.fontFamily()};
+  font-weight: ${theme.fontWeight('FW400')};
   text-decoration: none !important;
   ${({ severity }) => {
     switch (severity) {
       case Severity.INFO:
         return css`
-          color: ${theme.colour(Colour.BLUE, 'B50')};
-          background: ${theme.colour(Colour.BLUE, 'B30')};
+          &,
+          & * {
+            color: ${theme.colour(Colour.BLUE, 'B50')};
+            background: ${theme.colour(Colour.BLUE, 'B30')};
+          }
         `;
       case Severity.WARNING:
         return css`
-          color: ${theme.colour(Colour.YELLOW, 'Y70')};
-          background: ${theme.colour(Colour.YELLOW, 'Y20')};
+          &,
+          & * {
+            color: ${theme.colour(Colour.YELLOW, 'Y70')};
+            background: ${theme.colour(Colour.YELLOW, 'Y20')};
+          }
         `;
       case Severity.ERROR:
         return css`
-          color: ${theme.colour(Colour.RED, 'R50')};
-          background: ${theme.colour(Colour.RED, 'R20')};
+          &,
+          & * {
+            color: ${theme.colour(Colour.RED, 'R50')};
+            background: ${theme.colour(Colour.RED, 'R20')};
+          }
         `;
       default:
         return css`
-          color: ${theme.colour(Colour.GREEN, 'G50')};
-          background: ${theme.colour(Colour.GREEN, 'G20')};
+          &,
+          & * {
+            color: ${theme.colour(Colour.GREEN, 'G50')};
+            background: ${theme.colour(Colour.GREEN, 'G20')};
+          }
         `;
     }
   }}
@@ -44,29 +57,29 @@ const Alert = styled.div<Props>`
 
 const CheckIcon = styled(Check)`
   && {
-    font-size: ${theme.fontSize('FS20')};
-    margin-right: ${theme.spacing('S4')};
+    font-size: ${theme.fontSize('FS28')};
+    margin-right: ${theme.spacing('S6')};
   }
 `;
 
 const InfoIcon = styled(Info)`
   && {
-    font-size: ${theme.fontSize('FS20')};
-    margin-right: ${theme.spacing('S4')};
+    font-size: ${theme.fontSize('FS28')};
+    margin-right: ${theme.spacing('S6')};
   }
 `;
 
 const WarningIcon = styled(Warning)`
   && {
-    font-size: ${theme.fontSize('FS20')};
-    margin-right: ${theme.spacing('S4')};
+    font-size: ${theme.fontSize('FS28')};
+    margin-right: ${theme.spacing('S6')};
   }
 `;
 
 const ErrorIcon = styled(Error)`
   && {
-    font-size: ${theme.fontSize('FS20')};
-    margin-right: ${theme.spacing('S4')};
+    font-size: ${theme.fontSize('FS28')};
+    margin-right: ${theme.spacing('S6')};
   }
 `;
 
