10ef2002ba9cb2a21b44d1b8f98fe030d226f786
Jeff Reiffers <jeffreiffers@gmail.com>
2022-02-14 10:06:10 +0100

    chore: audit fix + remove graphql

---
+++ .storybook/main.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../packages/**/stories/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs'
+  ],
+  webpackFinal: async config => {
+    const assetRule = config.module.rules.find(({ test }) => test.test('.svg'));
+    assetRule.exclude = [path.resolve(__dirname, '../packages')];
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            configFile: path.resolve(__dirname, '../babel.config.js')
+          }
+        },
+        {
+          loader: 'react-svg-loader',
+          options: {
+            jsx: true
+          }
+        }
+      ],
+      include: [path.resolve(__dirname, '../packages')]
+    });
+    return config;
+  },
+  core: {
+    builder: 'webpack5'
+  }
+};
