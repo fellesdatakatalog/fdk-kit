89abcc84a40420e2e9a21f2fb2e24947e7670c64
Pooria Mehregan <pooriamehregan@gmail.com>
2022-09-28 15:10:20 +0200

    feat: add icon stories

---
+++ packages/icons/stories/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  > svg {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+export default { Container };
