4f72db6d9d92f42d6a9cc3b10529fd615a35088a
terjesyl <108797958+terjesyl@users.noreply.github.com>
2022-10-06 11:17:57 +0200

    feat: use new alert icons

--- packages/alert/src/components/alert/styled.ts
+++ packages/alert/src/components/alert/styled.ts
@@ -1,15 +1,10 @@
 import styled, { css } from 'styled-components';
 import { theme, Colour } from '@fellesdatakatalog/theme';
 
-import Check from '../../images/icon-alert-success-md.svg';
-import Info from '../../images/icon-alert-info-md.svg';
-import Warning from '../../images/icon-alert-warning-md.svg';
-import Error from '../../images/icon-alert-danger-md.svg';
-
 import { Severity } from './enums';
 
 interface Props {
-  severity?: Severity;
+  $severity?: Severity;
 }
 
 const Alert = styled.div<Props>`
@@ -21,8 +16,8 @@ const Alert = styled.div<Props>`
   font-family: ${theme.fontFamily()};
   font-weight: ${theme.fontWeight('FW400')};
   text-decoration: none !important;
-  ${({ severity }) => {
-    switch (severity) {
+  ${({ $severity }) => {
+    switch ($severity) {
       case Severity.INFO:
         return css`
           &,
@@ -59,52 +54,38 @@ const Alert = styled.div<Props>`
   }}
 `;
 
-const CheckIcon = styled(Check)`
-  && {
+const IconWrapper = styled.div<Props>`
+  & > svg {
     height: 24px;
     width: 24px;
-    margin-right: ${theme.spacing('S6')};
-    
-    & > path {
-      fill: ${theme.colour(Colour.GREEN, 'G50')};
+    & * {
+      stroke: ${({ $severity }) => {
+        switch ($severity) {
+          case Severity.INFO:
+            return css`
+              ${theme.colour(Colour.BLUE, 'B50')};
+            `;
+          case Severity.WARNING:
+            return css`
+              ${theme.colour(Colour.YELLOW, 'Y70')};
+            `;
+          case Severity.ERROR:
+            return css`
+              ${theme.colour(Colour.RED, 'R50')};
+            `;
+          default:
+            // SUCCESS
+            return css`
+              ${theme.colour(Colour.GREEN, 'G50')};
+            `;
+        }
+      }};
     }
   }
+  margin-right: ${theme.spacing('S6')};
 `;
 
-const InfoIcon = styled(Info)`
-  && {
-    height: 24px;
-    width: 24px;
-    margin-right: ${theme.spacing('S6')};
-    
-    & > path {
-      fill: ${theme.colour(Colour.BLUE, 'B50')};
-    }
-  }
-`;
-
-const WarningIcon = styled(Warning)`
-  && {
-    height: 24px;
-    width: 24px;
-    margin-right: ${theme.spacing('S6')};
-    
-    & > path {
-      fill: ${theme.colour(Colour.YELLOW, 'Y70')};
-    }
-  }
-`;
-
-const ErrorIcon = styled(Error)`
-  && {
-    height: 24px;
-    width: 24px;
-    margin-right: ${theme.spacing('S6')};
-    
-    & > path {
-      fill: ${theme.colour(Colour.RED, 'R50')};
-    }
-  }
-`;
-
-export default { Alert, CheckIcon, InfoIcon, WarningIcon, ErrorIcon };
+export default {
+  Alert,
+  IconWrapper
+};
