3a7b7e993f149180f120f2459a499c131e943393
Jeff Reiffers <jeffreiffers@gmail.com>
2023-01-12 11:10:51 +0100

    feat: external-footer language support

---
+++ packages/external-footer/src/utils/localization/en.ts
@@ -0,0 +1,21 @@
+export const en = {
+  searchInCatalogs: 'Search in catalogs',
+  searchInAllCatalogs: 'Search in all catalogs',
+  searchInDataset: 'Search in datasets',
+  searchInApis: 'Search in APIs',
+  searchInConcepts: 'Search in concepts',
+  searchInInformationModels: 'Search in informationmodels',
+  about: 'About',
+  aboutRegistration: 'Help to register',
+  guidance: 'Guides and standards',
+  reports: 'Reports',
+  organizations: 'Organizations',
+  news: 'News',
+  aboutTheWebsite: 'About the website',
+  directorateOfDigitizationManages:
+    'The Directorate of Digitization manages the National Data Catalog.',
+  privacyStatement: 'Privacy statement',
+  cookies: 'Cookies',
+  contact: 'Contact',
+  nationalDataCatalogOnTwitter: 'Twitter'
+};
---
+++ packages/external-footer/src/utils/localization/nn.ts
@@ -0,0 +1,21 @@
+export const nn = {
+  searchInCatalogs: 'Søk i katalogar',
+  searchInAllCatalogs: 'Søk i alle katalogar',
+  searchInDataset: 'Søk i datasett',
+  searchInApis: 'Søk i datatenester',
+  searchInConcepts: 'Søk i omgrep',
+  searchInInformationModels: 'Søk i informasjonsmodellar',
+  about: 'Om Felles datakatalog',
+  aboutRegistration: 'Hjelp til å registrera',
+  guidance: 'Rettleiarar og standardar',
+  reports: 'Rapportar',
+  organizations: 'Verksemder',
+  news: 'Aktuelt',
+  aboutTheWebsite: 'Om nettstaden',
+  directorateOfDigitizationManages:
+    'Digitaliseringsdirektoratet forvaltar Felles datakatalog.',
+  privacyStatement: 'Personvernerklæring',
+  cookies: 'Informasjonskapslar',
+  contact: 'Kontakt',
+  nationalDataCatalogOnTwitter: 'Felles datakatalog på Twitter'
+};
